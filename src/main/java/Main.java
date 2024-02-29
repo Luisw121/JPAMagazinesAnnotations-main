2eaaf4654a7a67fb237cf2c9b410c605c4060e49
Luisw121 <lafernandez@elpuig.xeill.net>
2024-02-29 17:37:27 +0100

    ULTIMOS CAMBIOS

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -60,7 +60,7 @@ public class Main {
             System.out.println("2. Eliminar Tablas. ");//LISTO
             System.out.println("3. Poblar Masivamente las tablas");//LISTO
             System.out.println("4. Mostrar Tablas");//
-            System.out.println("5. Seleccionar todos los elementos que contengan u ntexto en concreto");//
+            System.out.println("5. Seleccionar todos los elementos que contengan un texto en concreto");//
             System.out.println("6. Seleccionar un elemento en concreto y modificarlo");//
             System.out.println("7. Modificación de diferentes registros");//
             System.out.println("8. ELimincación de un registro en concreto");//
